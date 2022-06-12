07ae2459185050c660322532aea070160f71989a
Nicholas <nicholasjoven@gmail.com>
2022-06-12 17:26:43 -0400

    commit tree 470fd94ab7797e07ce8f7584710374e8d27f7d59

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
-}
+    const input = document.getElementById(`button`);
+
+function clickAlert() {
+    alert(`I was clicked!`);
+};
+
+input.addEventListener(`click`, clickAlert);
+};
+
+addingEventListener();
+
+// Important note: addingEventListener(); calls the function
+// addingEventListener only references it. Likewise clickAlert
+// is a reference but clickAlert() would trigger the function.
+
